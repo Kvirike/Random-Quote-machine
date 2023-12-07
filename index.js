96e0ab18ef9aa5b7d184c5646556f1d6b3b6ddae
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-12-08 02:22:57 +0400

    setTimeout()
    adding setTimeout function and some styles on markup

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ window.onload = init;
             generateQuote()
         }
         function generateQuote(){
-            let quoteSize = QUOTEBANK.length
-            let randomIndex = Math.floor(Math.random() * quoteSize);
-            let randomQuoteData = QUOTEBANK[randomIndex]
-            let twitterLink = `https://twitter.com/intent/tweet?hashtags=quotes&text=`
+            setTimeout(() => {
+                let quoteSize = QUOTEBANK.length
+                let randomIndex = Math.floor(Math.random() * quoteSize);
+                let randomQuoteData = QUOTEBANK[randomIndex]
+                let twitterLink = `https://twitter.com/intent/tweet?hashtags=quotes&text=`
 
-            // adding the quote
-            let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
-            twitterLink += quoteInApiFormat
-            // adding the author
-            let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20");
-            twitterLink += " - " + authorInApiFormat
-            
-            document.getElementById('tweet-quote').href  = twitterLink
-            document.getElementById("text").innerText = randomQuoteData.quote
-            document.getElementById("author").innerText = randomQuoteData.author
+                // adding the quote
+                let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
+                twitterLink += quoteInApiFormat
+                // adding the author
+                let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20");
+                twitterLink += " - " + authorInApiFormat
+                
+                document.getElementById('tweet-quote').href  = twitterLink
+                document.getElementById("text").innerText = randomQuoteData.quote
+                document.getElementById("author").innerText = randomQuoteData.author
+            }, 600);
         }
